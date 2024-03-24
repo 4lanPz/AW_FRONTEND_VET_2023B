c8706eec5fb48f9f80085afecab2a56609cb56f5
Alan Pérez <117743495+4lanPz@users.noreply.github.com>
2024-03-24 17:10:17 -0500

    Comentarios

--- src/context/TratamientosProvider.jsx
+++ src/context/TratamientosProvider.jsx
@@ -1,95 +1,100 @@
-import { createContext, useState } from "react";
-import axios from "axios";
+import { createContext, useState } from "react"; // Importa createContext y useState desde React
+import axios from "axios"; // Importa axios para realizar peticiones HTTP
 
+// Crea el contexto TratamientosContext
 const TratamientosContext = createContext();
 
+// Define el proveedor de contexto TratamientosProvider que proporciona contexto de tratamientos a los componentes hijos
 const TratamientosProvider = ({ children }) => {
+  // Define estados para el modal, mensaje y tratamientos
   const [modal, setModal] = useState(false);
   const [mensaje, setMensaje] = useState({});
   const [tratamientos, setTratamientos] = useState([]);
 
+  // Función para manejar el estado del modal
   const handleModal = () => {
     setModal(!modal);
   };
 
+  // Función para registrar tratamientos
   const registrarTratamientos = async (datos) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token"); // Obtiene el token de autenticación del almacenamiento local
     try {
-      const url = `${import.meta.env.VITE_BACKEND_URL}/tratamiento/registro`;
+      const url = `${import.meta.env.VITE_BACKEND_URL}/tratamiento/registro`; // URL para la solicitud de registro de tratamientos
       const options = {
         headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json", // Tipo de contenido de la solicitud
+          Authorization: `Bearer ${token}`, // Encabezado de autorización con el token JWT
         },
       };
-      const respuesta = await axios.post(url, datos, options);
-      setTratamientos([respuesta.data.tratamiento, ...tratamientos]);
+      const respuesta = await axios.post(url, datos, options); // Realiza la solicitud POST para registrar tratamientos
+      setTratamientos([respuesta.data.tratamiento, ...tratamientos]); // Actualiza la lista de tratamientos con el nuevo tratamiento registrado
     } catch (error) {
-      console.log(error);
+      console.log(error); // Maneja el error en caso de que falle la solicitud
     }
   };
 
+  // Función para eliminar un tratamiento
   const handleDelete = async (id) => {
     try {
       const confirmar = confirm(
         "Vas a eliminar el tratamiento de un paciente, ¿Estás seguro de realizar esta acción?"
       );
       if (confirmar) {
-        const token = localStorage.getItem("token");
-        const url = `${import.meta.env.VITE_BACKEND_URL}/tratamiento/${id}`;
+        const token = localStorage.getItem("token"); // Obtiene el token de autenticación del almacenamiento local
+        const url = `${import.meta.env.VITE_BACKEND_URL}/tratamiento/${id}`; // URL para la solicitud de eliminación de tratamiento
         const options = {
           headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json", // Tipo de contenido de la solicitud
+            Authorization: `Bearer ${token}`, // Encabezado de autorización con el token JWT
           },
         };
-        const response = await axios.delete(url, options);
+        const response = await axios.delete(url, options); // Realiza la solicitud DELETE para eliminar el tratamiento
         const tratamientosActualizados = tratamientos.filter(
           (tratamiento) => tratamiento._id !== id
-        );
-        setTratamientos(tratamientosActualizados);
-        setMensaje({ respuesta: response.data?.msg, tipo: true });
+        ); // Filtra los tratamientos para actualizar la lista
+        setTratamientos(tratamientosActualizados); // Actualiza la lista de tratamientos
+        setMensaje({ respuesta: response.data?.msg, tipo: true }); // Establece el mensaje de éxito
         setTimeout(() => {
           setMensaje({});
-        }, 2000);
+        }, 2000); // Borra el mensaje después de 2 segundos
       }
     } catch (error) {
-      setMensaje({ respuesta: response.data?.msg, tipo: false });
+      setMensaje({ respuesta: error.response?.data?.msg, tipo: false }); // Establece el mensaje de error
     }
   };
 
+  // Función para manejar el estado de un tratamiento
   const handleStatus = async (id) => {
-    const token = localStorage.getItem("token");
+    const token = localStorage.getItem("token"); // Obtiene el token de autenticación del almacenamiento local
     try {
       const confirmar = confirm(
         "Vas a finalizar el tratamiento de un paciente, ¿Estás seguro de realizar esta acción?"
       );
       if (confirmar) {
-        const url = `${
-          import.meta.env.VITE_BACKEND_URL
-        }/tratamiento/estado/${id}`;
+        const url = `${import.meta.env.VITE_BACKEND_URL}/tratamiento/estado/${id}`; // URL para la solicitud de cambio de estado de tratamiento
         const options = {
           headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json", // Tipo de contenido de la solicitud
+            Authorization: `Bearer ${token}`, // Encabezado de autorización con el token JWT
           },
         };
-        const response = await axios.put(url, {}, options);
+        const response = await axios.put(url, {}, options); // Realiza la solicitud PUT para cambiar el estado del tratamiento
         const tratamientosActualizados = tratamientos.filter(
           (tratamiento) => tratamiento._id !== id
-        );
-        setTratamientos(tratamientosActualizados);
-        setMensaje({ respuesta: response.data?.msg, tipo: false });
+        ); // Filtra los tratamientos para actualizar la lista
+        setTratamientos(tratamientosActualizados); // Actualiza la lista de tratamientos
+        setMensaje({ respuesta: response.data?.msg, tipo: false }); // Establece el mensaje de éxito
         setTimeout(() => {
           setMensaje({});
-        }, 2000);
+        }, 2000); // Borra el mensaje después de 2 segundos
       }
     } catch (error) {
-      // Use the 'error' parameter instead of 'response'
-      setMensaje({ respuesta: error.response?.data?.msg, tipo: false });
+      setMensaje({ respuesta: error.response?.data?.msg, tipo: false }); // Establece el mensaje de error
     }
   };
 
+  // Retorna el proveedor de contexto con los valores y funciones necesarios
   return (
     <TratamientosContext.Provider
       value={{
@@ -108,5 +113,9 @@ const TratamientosProvider = ({ children }) => {
     </TratamientosContext.Provider>
   );
 };
+
+// Exporta el proveedor de contexto TratamientosProvider
 export { TratamientosProvider };
+
+// Exporta el contexto TratamientosContext
 export default TratamientosContext;
